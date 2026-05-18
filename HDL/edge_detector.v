23bea346ad144a8c583ea1fb5001b8b201eddc18
Joross Juele <jorossjuele527@gmail.com>
2026-05-18 20:14:47 +0800

    Update edge_detector.v
    replace if allowed

--- HDL/edge_detector.v
+++ HDL/edge_detector.v
@@ -17,3 +17,17 @@ module edge_detector (
 
     assign btn_pulse = sw_curr & ~sw_prev;
 endmodule
+
+/*reg sync_0, sync_1;
+    reg sw_prev;
+
+    always @(posedge clk) begin
+        sync_0  <= sw;     // Stage 1: Catch asynchronous input
+        sync_1  <= sync_0; // Stage 2: Stabilize signal to clock domain
+        sw_prev <= sync_1; // Stage 3: Delay for edge comparison
+    end
+
+    // High for exactly one clock cycle when transition completes cleanly
+    assign btn_pulse = sync_1 & ~sw_prev;
+endmodule 
+*/
